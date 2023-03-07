6daf963ef4f0a95f0a177e0a71df6ec75efd67f9
Ybertheau <yannick.bertheau@hotmail.fr>
2023-03-07 11:58:15 +0100

    spring boot

---
+++ alten-shop-back-products/back/src/main/java/Entreprise/exoProduct/ExoProductApplication.java
@@ -0,0 +1,13 @@
+package Entreprise.exoProduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExoProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExoProductApplication.class, args);
+	}
+
+}
